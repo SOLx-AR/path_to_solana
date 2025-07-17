f954ef4988e281209cb94c3b511c0466097c9f60
juanito87 <juancruz.f87@gmail.com>
2025-07-17 20:42:19 -0300

    Sacando gpg porque no funciona y era solo un ejemplo

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+cd /workspaces/path_to_solana/.devcontainer || exit
 
 printf "Ejecutar configuración de neovim.\n"
 ./nvim-setup.sh
-printf "Ejecutar configuración de gpg.\n"
-./gpg-setup.sh
